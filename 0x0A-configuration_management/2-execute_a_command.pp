9673182dd023e08ce10c923bdc604edfb21f48f7
jaarmore <jaarmore@gmail.com>
2020-03-24 00:10:10 +0000

    changin attributes from file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-    provider => 'shell',
+    path     => '/usr/bin/',
     command  => '/usr/bin/pkill -f killmenow',
 }
