4adaf2b2f97fde2721996f95151bacc970b7708c
jaarmore <jaarmore@gmail.com>
2020-03-24 00:05:47 +0000

    fix code on file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
+    provider => 'shell',
     path     => '/usr/bin/',
     command  => '/usr/bin/pkill -f killmenow',
-    provider => 'shell',
 }
