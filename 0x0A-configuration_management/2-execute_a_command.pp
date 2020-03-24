0e0256d94d3e6ee7715aee9336ebf5875b983396
jaarmore <jaarmore@gmail.com>
2020-03-24 00:20:25 +0000

    fix indentation on file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-    path     => '/usr/bin/',
-    command  => '/usr/bin/pkill -f killmenow',
+    path    => '/usr/bin/',
+    command => '/usr/bin/pkill -f killmenow',
 }
