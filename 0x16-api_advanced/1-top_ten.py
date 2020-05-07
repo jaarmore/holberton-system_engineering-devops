5f761287e73d0db0706c23dc384feb5894d30b9b
jaarmore <jaarmore@gmail.com>
2020-05-07 23:56:22 +0000

    Remove trailing space in line 3

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and prints the 
+Function that queries the Reddit API and prints the
 titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
