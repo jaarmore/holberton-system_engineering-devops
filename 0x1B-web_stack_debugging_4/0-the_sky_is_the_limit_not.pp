8a10215158c1f5db20b9c1bbc7c093cbb3754beb
jaarmore <jaarmore@gmail.com>
2020-05-28 16:36:41 +0000

    fix -n on file

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 # Settings worker_rlimit_nofile
 exec { 'fix--for-nginx':
-  command => "sed -i 's/-n 15/n 10000/g' /etc/default/nginx",
+  command => "sed -i 's/-n 15/-n 10000/g' /etc/default/nginx",
   path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
 
